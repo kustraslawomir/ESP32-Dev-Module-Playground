967df63a0c95f4088648481ccb812189e3d80926
skustra <kustraslawomir@gmail.com>
2018-11-20 18:55:09 +0100

    run BLE server on ESP32 device

---
+++ src/Temprerature.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+class Tempterature {
+
+public:
+  void measureTemperature(){
+    cout << "measure temperature" << endl;
+  };
+
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,34 @@
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
 #include <Arduino.h>
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
 
-int ledBuiltIn = 2;
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
 void setup() {
-  pinMode(ledBuiltIn, OUTPUT);
+  Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+
+  BLEDevice::init("ESP LAMP");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  pCharacteristic->setValue("Hello World says Neil");
+  pService->start();
+  BLEAdvertising *pAdvertising = pServer->getAdvertising();
+  pAdvertising->start();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
 }
 
 void loop() {
-  digitalWrite(ledBuiltIn, HIGH);
-  delay(1000);
-  digitalWrite(ledBuiltIn, LOW);
-  delay(1000);
-}
-
-void blinkBlueLed(){
-
+   Serial.println("Hello World!");
+  delay(2000);
 }

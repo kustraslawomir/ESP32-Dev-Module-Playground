179fb569ad16db04c1de447aa52bb96a687e11f8
skustra <kustraslawomir@gmail.com>
2018-11-27 22:33:46 +0100

    updated BLE Server for Tx/Rx communication

--- src/main.cpp
+++ src/main.cpp
@@ -2,53 +2,112 @@
 #include <BLEUtils.h>
 #include <BLEServer.h>
 #include <Arduino.h>
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
-char incomingByte;
-int  LED = 2;
+#include <BLE2902.h>
 
-#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
-void setup() {
+BLEDescriptor *pDescriptor;
+BLECharacteristic *pCharacteristic;
+
+const int bluePin = 2;
+
+uint8_t txValue = 0;
+
+bool deviceConnected = false;
+bool deviceNotifying = false;
+
+#define SERVICE_UUID "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string rxValue = pCharacteristic->getValue();
+
+      if (rxValue.length() > 0) {
+        Serial.println("*********");
+        Serial.print("Received Value: ");
+        for (int i = 0; i < rxValue.length(); i++)
+          Serial.print(rxValue[i]);
+
+        Serial.println();
+        Serial.println("*********");
+      }
+    }
+};
+
+class MyDisCallbacks: public BLEDescriptorCallbacks {
+    void onWrite(BLEDescriptor *pDescriptor) {
+      uint8_t* rxValue = pDescriptor->getValue();
+
+     if (pDescriptor->getLength() > 0) {
+        if (rxValue[0]==1) {
+          //deviceNotifying=true;
+        } else {
+          deviceNotifying=false;
+        }
+
+      if (pDescriptor->getLength() > 0) {
+        Serial.println("*********");
+        Serial.print("Received Descriptor Value: ");
+        for (int i = 0; i < pDescriptor->getLength(); i++)
+          Serial.print(rxValue[i]);
+
+        Serial.println();
+        Serial.println("*********");
+      }
+    }
+  };
+};
+
+class MyBLEServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+void setup(){
   Serial.begin(115200);
-  Serial.println("Starting BLE");
-
   BLEDevice::init("ESP_LAMP");
   BLEServer *pServer = BLEDevice::createServer();
+  //*pServer = pServer  ->setCallbacks(new MyBLEServerCallbacks());
   BLEService *pService = pServer->createService(SERVICE_UUID);
-  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
-                                         CHARACTERISTIC_UUID,
-                                         BLECharacteristic::PROPERTY_READ |
-                                         BLECharacteristic::PROPERTY_WRITE);
 
-  pCharacteristic->setValue("Hello");
+  pCharacteristic  = pService->createCharacteristic(
+                    CHARACTERISTIC_UUID_TX,
+                    BLECharacteristic::PROPERTY_NOTIFY
+                  );
+
+  pDescriptor = new BLE2902();
+  pCharacteristic->addDescriptor(pDescriptor);
+
+  BLECharacteristic *rXCharacteristic = pService->createCharacteristic(
+                                       CHARACTERISTIC_UUID_RX,
+                                       BLECharacteristic::PROPERTY_WRITE);
+
+  rXCharacteristic->setCallbacks(new MyCallbacks());
+  pDescriptor->setCallbacks(new MyDisCallbacks());
+
+
   pService->start();
-  BLEAdvertising *pAdvertising = pServer->getAdvertising();
-  pAdvertising->start();
-  Serial.println("Characteristic defined! Now you can read it in your phone!");
+  pServer->getAdvertising()->start();
 
-  Serial.begin(9600);
-  pinMode(LED, OUTPUT);
-  Serial.println("Press 1 to LED ON or 0 to LED OFF...");
-
+  Serial.println("Waiting to notify...");
 }
 
-void loop() {
-    delay(20);
-Serial.println("YY");
-  if (Serial.available()>0) {
+void loop(){
+  if(Serial.available() || Serial.available()>0){
+    Serial.println("TEST");
+  }
 
-   incomingByte = Serial.read();
-Serial.println("TEST");
-   if(incomingByte == '0') {
-      digitalWrite(LED, LOW);
-      Serial.println("LED OFF. Press 1 to LED ON!");
-   }
-   else {
-      digitalWrite(LED, HIGH);
-      Serial.println("LED ON. Press 0 to LED OFF!");
-   }
- }
- delay(2000);
+  if (deviceConnected && deviceNotifying) {
+    Serial.printf("*** Sent Value: %d ***\n", txValue);
+    pCharacteristic->setValue(&txValue, 1);
+    pCharacteristic->notify();
+    txValue++;
+  }
+   delay(1000);
 }
